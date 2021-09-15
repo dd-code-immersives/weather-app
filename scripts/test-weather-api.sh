eae1d939c682635c31dd4f12c4216c0c495ed55c
dd-code-immersives <domilamad89@gmail.com>
2021-09-15 12:39:53 -0600

    added command line argument to bash script

--- scripts/test-weather-api.sh
+++ scripts/test-weather-api.sh
@@ -1,1 +1,1 @@
-curl "http://api.openweathermap.org/data/2.5/weather?appid=c65f07a477ed814d4725adf9eabb948b&q=San Diego"
+curl "http://api.openweathermap.org/data/2.5/weather?appid=c65f07a477ed814d4725adf9eabb948b&q=$1"
