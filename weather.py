87ae6bf73ed98f5b68f92035811508ed13270fed
TonyPittella <atpittella@gmail.com>
2021-09-15 16:56:53 -0400

    Added multi-city functionality cleaned it up

--- weather.py
+++ weather.py
@@ -1,34 +1,39 @@
-import requests 
+import requests
+import csv
 
-# Enter your API key here 
 api_key = "c65f07a477ed814d4725adf9eabb948b"
-  
-# base_url variable to store url 
-base_url = "http://api.openweathermap.org/data/2.5/weather?"
-  
-# Give city name 
-city_name = input("Enter city name : ") 
-   
-complete_url = base_url + "appid=" + api_key + "&q=" + city_name 
-response = requests.get(complete_url) 
-x = response.json() 
 
-if x["cod"] != "404": 
-  
-    y = x["main"] 
-    current_temperature = y["temp"] 
-    current_pressure = y["pressure"] 
-    current_humidiy = y["humidity"]
-    z = x["weather"] 
-    weather_description = z[0]["description"] 
-    print(" Temperature (in kelvin unit) = " +
-                    str(current_temperature) + 
-          "\n atmospheric pressure (in hPa unit) = " +
-                    str(current_pressure) +
-          "\n humidity (in percentage) = " +
-                    str(current_humidiy) +
-          "\n description = " +
-                    str(weather_description)) 
-  
-else: 
-    print(" City Not Found ")
+base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
+
+def get_weather(city_name, flags):
+    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+    response = requests.get(complete_url)
+    x = response.json()
+    if x["cod"] != "404":
+        y = x["main"]
+        current_temperature = y["temp"]
+        current_pressure = y["pressure"]
+        current_humidity = y["humidity"]
+        z = x["weather"]
+        weather_description = z[0]["description"]
+        if len(flags) > 0:
+            for flag in flags:
+                if flag == "temp":
+                    print(" Temperature YOU WANT(in kelvin unit) = " +
+                          str(current_temperature))
+                if flag == "pressure":
+                    print(
+                        "Atmospheric pressure YOU WANT (in hPa unit) = " + str(current_pressure))
+                if flag == "hum":
+                    print("Humidity YOU WANT (in percentage) = " +
+                          str(current_humidity))
+                if flag == "desc":
+                    print("Description YOU WANT = " + str(weather_description))
+        else:
+            print("Temperature (in kelvin unit) = " + str(current_temperature))
+            print("Atmospheric pressure (in hPa unit) = " + str(current_pressure))
+            print("Humidity (in percentage) = " + str(current_humidity))
+            print("Description = " + str(weather_description))
+    else:
+        print(" City Not Found ")
